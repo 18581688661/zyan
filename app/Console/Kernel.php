e536d8b932b865dffdcac742fd1eb6296db8c042
Jackson <240728816@qq.com>
2017-09-01 17:21:44 +0800

    完善每日提醒

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,11 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
+use Auth;
+use Mail;
 
 class Kernel extends ConsoleKernel
 {
@@ -28,17 +33,23 @@ class Kernel extends ConsoleKernel
         //          ->dailyAt('12:00')
         //          ->sendOutputTo('/home/wwwroot/zyan/storage/framework/renwu')
         //          ->emailOutputTo('240728816@qq.com');
+        
         $schedule->call(function(){
+        $user=User::findOrFail(1);
+        $this->sendEmailConfirmationTo($user);
+        })->everyMinute();
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
         $view = 'emails.tixing';
-        // $data = compact('user');
+        $data = compact('user');
         $from = '18581688661@163.com';
         $name = '【勤智数码】';
         $to = '240728816@qq.com';
         $subject = "【勤智数码】工时登记提醒";
-
-        Mail::send($view, function ($message) use ($from, $name, $to, $subject) {
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
             $message->from($from, $name)->to($to)->subject($subject);
         });
-        })->everyMinute();
     }
 }
